09e70c02299e5629093461eee35c9353e6cab48a
hewenhao-hwh <282106931@qq.com>
2018-02-28 20:57:45 +0800

    refresh

--- js/main.js
+++ js/main.js
@@ -4,10 +4,8 @@
 $(function(){
 	$(window).resize();
 	$("#block-nav").css("z-index", 1);
-
 	// 导航条固定顶部
 	$("#block-nav").navFixed();
-
 	//平滑滚动导航
 	$('#fstPage-down a, nav a, #logo').bind('click',function(event){
 		var $anchor = $(this);
@@ -24,7 +22,6 @@ $(window).resize(function(){
 	$('.blockTitle').stop().fadeIn("normal").animate({
 		"top" : ($(window).height() - $('.blockTitle').outerHeight())/2
 	},500); 
-
 	$("#block-wantMore").css("height", $(window).height()-52 + "px");
 	$('#block-wantMore>p').css("top", ($("#block-wantMore").outerHeight(true) - $('#block-wantMore>p').outerHeight())/2 + "px"); 
 });
